36f100792d5b7ccacbf3c016ea543e1954b10266
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-09-25 07:17:26 -0500

    middlewares
    Middlewares

--- .history/models/pageView_20190408190907.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const pageViewSchema = new mongoose.Schema({
-  path: { type: String },
-  userAgent: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model("pageView", pageViewSchema);
